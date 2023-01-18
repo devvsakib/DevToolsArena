dddc054c95b1c2f6b6a43c527e3663884f00b2e6
DevvSakib <devvsakib@gmail.com>
2023-01-18 12:48:47 +0600

    error solution line85 break fixed

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -34,21 +34,20 @@ function SearchInput({ search, setSearch, setType }) {
                         <ul className={`flex flex-col sm:flex-row mx-auto mt-2 items-start gap-4 py-3 px-6 rounded-lg bg-white w-full md:w-12/12  md:w-auto`}>
                             {
                                 errorType.map((item, i) => (
-                                    <li 
-                                    key={i} 
-                                    className={`${
-                                        item === "add"
-                                        ? "bg-[#4024e0]"
-                                        : item === "commit"
-                                        ? "bg-[#1a5ba5]"
-                                        : item === "merge"
-                                        ? "bg-[#118d7c]"
-                                        : item === "push"
-                                        ? "bg-[#8d54e1]"
-                                        : item === "branch"
-                                        ? "bg-[#40E4F0]"
-                                        : "bg-[#7e1aa5]"
-                                    }
+                                    <li
+                                        key={i}
+                                        className={`${item === "add"
+                                                ? "bg-[#4024e0]"
+                                                : item === "commit"
+                                                    ? "bg-[#1a5ba5]"
+                                                    : item === "merge"
+                                                        ? "bg-[#118d7c]"
+                                                        : item === "push"
+                                                            ? "bg-[#8d54e1]"
+                                                            : item === "branch"
+                                                                ? "bg-[#40E4F0]"
+                                                                : "bg-[#7e1aa5]"
+                                            }
                                          w-full md:w-auto rounded-md text-white font-bold py-1 px-3 cursor-pointer`}
                                         onClick={() => setType(item)}
                                     >{item}</li>
@@ -61,15 +60,15 @@ function SearchInput({ search, setSearch, setType }) {
                                 onClick={() => setOpen(!open)}>
                                 Filter By Type
                             </button>
-                              <ul className={`animate flex flex-col sm:flex-row mx-auto mt-2 items-start gap-4 py-3 pt-4 px-6 rounded-lg bg-white w-[60vw] md:w-auto text-left ${open ? "hidden" : "block"}`}>
-                                    {
-                                        errorType.map((item, i) => (
-                                            <li key={i} className={`${item === "add" ? "bg-[#4024e0]" : item === "commit" ? "bg-[#1a5ba5]" : item === "push" ? "bg-[#1aa0a5]" : "bg-[#7e1aa5]"} w-full md:w-auto rounded-md text-white font-bold py-2 px-3 cursor-pointer`}
-                                                onClick={() => {setType(item); setOpen(!open)}}
-                                            >{item}</li>
-                                        ))
-                                    }
-                                </ul>
+                            <ul className={`animate flex flex-col sm:flex-row mx-auto mt-2 items-start gap-4 py-3 pt-4 px-6 rounded-lg bg-white w-[60vw] md:w-auto text-left ${open ? "hidden" : "block"}`}>
+                                {
+                                    errorType.map((item, i) => (
+                                        <li key={i} className={`${item === "add" ? "bg-[#4024e0]" : item === "commit" ? "bg-[#1a5ba5]" : item === "push" ? "bg-[#1aa0a5]" : "bg-[#7e1aa5]"} w-full md:w-auto rounded-md text-white font-bold py-2 px-3 cursor-pointer`}
+                                            onClick={() => { setType(item); setOpen(!open) }}
+                                        >{item}</li>
+                                    ))
+                                }
+                            </ul>
                         </div>
                     )
                 }
