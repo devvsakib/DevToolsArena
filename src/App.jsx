805c02aff9a19271664604a598e049a65c60097f
DevvSakib <devvsakib@gmail.com>
2023-01-23 15:48:25 +0600

    deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
-
+import Doc from "./pages/Doc";
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: '/doc',
+      element: <Doc />
+    }
+  ]
+);
 function App() {
   return (
-    <Home />
+    <RouterProvider router={router} />
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Doc from "./pages/Doc";
+
 
 /* Creating a router object that is used to render the correct component based on the url. */
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-    },
-    {
-      path: '/doc',
-      element: <Doc />
-    }
-  ]
-);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
