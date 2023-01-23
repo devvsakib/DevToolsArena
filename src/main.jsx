b41c5fca9c49cd2b3d5eeaa28db0f4ba31522261
DevvSakib <devvsakib@gmail.com>
2023-01-23 15:52:21 +0600

    deploy error

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-
-/* Creating a router object that is used to render the correct component based on the url. */
-
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
