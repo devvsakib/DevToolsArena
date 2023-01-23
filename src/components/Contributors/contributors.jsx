108b3e17e62573d137c1538fbb5f3fbbf6393d21
ICE988 <mohamedsamir22446688@gmail.com>
2023-01-23 20:40:56 +0200

    finished all

--- src/components/Contributors/contributors.jsx
+++ src/components/Contributors/contributors.jsx
@@ -11,10 +11,11 @@ function Contribute() {
           id="container"
           className="py-4 mb-4 col-span-12 md:col-span-6 xl:col-span-4 px-2 md:px-6 border-l-4 rounded-lg   items-start bg-dark-secondary flex flex-col flex mt-8 items-center gap-2 px-3 py-2 border border-gray rounded-lg hover:border-primary hover:text-primary">
             <h1>{data.name}</h1>
-            <img src={data.image} />
+            <img className='pfp' src={data.image} />
             <div className="links">
-              <a href={data.github}>Github</a>
-              <a href={data.twiter}>Twiter</a>
+              <a className='spf-link' href={data.github}>Github</a>
+              <a className='spf-link' href={data.twiter}>Twiter</a>
+              {/* spf-link : abbreviation of (specific-link) for css */}
             </div>
           </div>
           )
