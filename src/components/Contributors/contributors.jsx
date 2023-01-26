0f2694941fa9a5d7e3d9296d17d113a859ff3aa6
DevvSakib <devvsakib@gmail.com>
2023-01-26 15:34:31 +0600

    img path fixed

--- src/components/Contributors/contributors.jsx
+++ src/components/Contributors/contributors.jsx
@@ -47,7 +47,7 @@ function Contribute() {
           )
           ) : (
             <div className='translate-y-[4rem]'>
-              <img src="public/assets/run.gif" 
+              <img src="/public/assets/run.gif" 
               className='w-1/2 mx-auto'
               alt="Running Gif" />
               <h1 className='text-center md:text-2xl text-white'>Contributors coming...</h1>
